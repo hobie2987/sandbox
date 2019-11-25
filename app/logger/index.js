bbd8e6674114a24ec367445ef118602a5bb235a2
hobie2987 <hobie2987@gmail.com>
2019-11-24 22:04:38 -0500

    feat(logger): export Logger and LogCodes

--- app/logger/index.js
+++ app/logger/index.js
@@ -2,5 +2,7 @@
 const LoggerRoute = require('./logger.route');
 const LoggerValidator = require('./logger.validator');
 const LoggerMiddleware = require('./logger.middleware');
+const Logger = require('./logger.service.');
+const LogCodes = require('./log-codes.js');
 
-module.exports = { LoggerRoute, LoggerValidator, LoggerMiddleware };
+module.exports = { LoggerRoute, LoggerValidator, LoggerMiddleware, Logger, LogCodes };
