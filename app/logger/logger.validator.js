06eb4f4c17db888d73ff015a5766494cbc44702a
hobie2987 <hobie2987@gmail.com>
2019-11-24 22:01:41 -0500

    feat(logger): remove data validation

--- app/logger/logger.validator.js
+++ app/logger/logger.validator.js
@@ -1,22 +1,8 @@
 /*globals module:true, require:true */
-const { body, sanitizeBody } = require('express-validator');
-const { CustomSanitizers } = require('../request-validator')
+const { body } = require('express-validator');
 
 module.exports = [
-    body('log-code', 'log-code must be numeric').isNumeric(),
-    body('level').exists().isString().not().isEmpty().isIn(['info', 'error', 'warn']),
-    body('message').optional().isString(),
-    body('data').exists().isArray({min: 1}),
-    body('data.*.name', 'name must be a non-empty string')
-        .exists()
-        .isString()
-        .not().isEmpty()
-        .bail()
-        .customSanitizer(CustomSanitizers.toLowerCase),
-    body('data.*.employer', 'employer must be a non-empty string')
-        .exists()
-        .isString()
-        .not().isEmpty()
-        .bail()
-        .customSanitizer(CustomSanitizers.removeSpecialChars)
+    body('log-code', 'log-code must be numeric').isNumeric().toInt(),
+    body('level').isString().isIn(['info', 'error', 'warn']),
+    body('message').optional().isString()
 ];
