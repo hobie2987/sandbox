ce9774893627a0f88113e5c7138994c4b9c13cb5
hobie2987 <hobie2987@gmail.com>
2019-11-24 22:00:06 -0500

    feat(file-upload): validate content type; add files to request

--- app/file-upload/file-upload.middleware.js
+++ app/file-upload/file-upload.middleware.js
@@ -1,22 +1,37 @@
 /*globals module, require*/
 const Busboy = require('busboy');
+const LogCodes = require('../logger/log-codes');
 
 module.exports = (request, response, next) => {
 
-    request.files = request.files || {};
-
-    if (request.method !== 'POST') {
+    const contentType = request.headers['content-type'];
+    if (!contentType.startsWith('multipart/form-data')) {
         next();
         return;
     }
 
+    request.files = request.files || {};
+
+    const logger = request.logger;
     const busboy = new Busboy({headers: request.headers});
+    request.pipe(busboy);
 
-    busboy.on('file', (fieldName, file, filename, encoding, mimetype) => {
+    busboy.on('file', function(fieldName, file, fileName, encoding, mimeType) {
 
+        file.on('data', function(data) {
+            logger.info({
+                fieldName: fieldName,
+                fileName: fileName,
+                encoding: encoding,
+                mimetype: mimeType,
+                bytes: data.length
+            }, LogCodes.FILE_UPLOAD);
+
+            request.files[fileName] = data;
+        });
     });
 
-    busboy.on('finish', () => {
+    busboy.on('finish', function() {
         next();
     });
 };
