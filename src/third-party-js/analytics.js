d7aea0f0087120f358537279c30b9a42e486e617
hobie2987 <hobie2987@gmail.com>
2019-11-24 21:55:06 -0500

    feat(analytics): remove data from logs

--- src/third-party-js/analytics.js
+++ src/third-party-js/analytics.js
@@ -1,14 +1,7 @@
-// window.alert('Im here!');
-
-
 const data = {
     message: 'Test of log endpoint',
     'log-code': 100,
-    level: 'info',
-    data: [
-        { name: 'Eric', employer: 'Vanguard' },
-        { name: 'Colin', employer: 'Chic-fil-A Inc.' }
-    ]
+    level: 'info'
 };
 
 $.ajax({
@@ -16,9 +9,6 @@ $.ajax({
     url: '/sandbox/log',
     contentType : 'application/json',
     data: JSON.stringify(data),
-    headers: {
-        'X-XSRF-TOKEN': $.cookie('XSRF-TOKEN')
-    },
     success: function(result) {
         console.log(result)
     }
