2ba6c4b8b301fdb6b12671ee5d9c97cfde6ac6f6
hobie2987 <hobie2987@gmail.com>
2019-11-24 21:56:28 -0500

    feat(grunt): copy json to dist

--- grunt/copy.js
+++ grunt/copy.js
@@ -1,9 +1,10 @@
 module.exports = {
     dist: {
         files: [
-            {expand: true, flatten: true, cwd: 'src', flatten: true, src: ['**/*.css'], dest: 'dist'},
+            {expand: true, flatten: true, cwd: 'src', src: ['**/*.css'], dest: 'dist'},
             {expand: true, flatten: true, cwd: 'src', src: ['**/*.js'], dest: 'dist'},
-            {expand: true, flatten: true, cwd: 'src', src: ['favicon.ico'], dest: 'dist'}
+            {expand: true, flatten: true, cwd: 'src', src: ['favicon.ico'], dest: 'dist'},
+            {expand: true, flatten: true, cwd: 'app/json', src: ['**/*.json'], dest: 'dist/json'}
         ]
     }
 };
