9b067ec74d605384c4a2a57c8ddf26a26e9ea29b
hobie2987 <hobie2987@gmail.com>
2019-11-24 22:05:58 -0500

    feat(filter): request filters middleware

---
+++ app/filters/filter.router.js
@@ -0,0 +1,44 @@
+const router = require('express').Router();
+
+/**
+ * Generates a unique sessions for each user. Generates
+ * a session cookie which is used to populate the session
+ * on each request.  The active session can be accessed by
+ * the following: request.session
+ */
+router.use(require('../session/session.middleware'));
+
+/**
+ * Attaches a logger instance to each request to be
+ * utilized within other middleware. The instance
+ * can be accessed by the following: request.logger
+ */
+router.use(require('../logger/logger.middleware'));
+
+/**
+ * Generates info logs containing statistics of the
+ * current application memory usage.
+ */
+router.use(require('../memory-monitor/memory-monitor.middleware'));
+
+/**
+ * Validates the XSRF-TOKEN cookie matches the token
+ * saved on the users session (excluding GET requests).
+ * If no token exists on session, one is created and
+ * updated on the users session and set on the XSRF-TOKEN
+ * cookie.
+ */
+router.use(require('../csrf/csrf.middleware.js'));
+
+/**
+ * Parses the files submitted to the server and places
+ * them on: request.files
+ */
+router.post('/*', require('../file-upload/file-upload.middleware.js'));
+
+/**
+ * Add CSP and no-cache headers to GET requests
+ */
+router.get('/*', require('../headers/headers.middleware'));
+
+module.exports = router;
